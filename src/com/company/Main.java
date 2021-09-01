4d6e6c373cedf70a41ebc630b806e5c34371da92
Merve <mervetutoglu817@gmail.com>
2021-09-01 20:14:53 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,36 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int result = 0;
+	int y = 1;
+	int x = 1;
+	int m = 1;
+        {
+	if (y < 0) {
+        System.out.println(y *= -1);
+        System.out.println(m = -1);
+    }
+     else    {
+        System.out.println(m = 1);
+     return; }
+
+     while (y > 0)
+         if (y % 2 == 0){
+             System.out.println(x = x + x);
+            System.out.println(y = y / 2); }
+            else {
+             System.out.println(result = result + x);
+            y = y - 1;
+            return; }
+
+            if (m < 0)
+                System.out.println(result *= -1);
+            System.out.println(result);
+return;
+    }
+
+    }
+
+}
